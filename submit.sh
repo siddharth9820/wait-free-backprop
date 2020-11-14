67fd66593d9d9cd1a56c46c2f7fcbc2d2540150c
ocnkr <ocankur@umd.edu>
2020-11-14 17:25:03 -0500

    update submit.sh

--- submit.sh
+++ submit.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-#SBATCH --ntasks 2
+
+#SBATCH -N 2
 #SBATCH --ntasks-per-node 1 
 #SBATCH -t 10:00
 #SBATCH --gres=gpu:2
 #SBATCH --constraint="rhel8"
-#cd /homes/cmsc818x-1uz9/wait-free-backprop/nccl 
+
+cd /homes/cmsc818x-1uz9/wait-free-backprop/
 
 NCCL_DEBUG=INFO mpirun -n 2 ./multi_gpu 
