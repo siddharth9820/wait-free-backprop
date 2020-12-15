6cd4326a0e47540d705921f178aeebdcf7a09a5e
Joy Kitson <jkitson@umd.edu>
2020-12-14 21:28:21 -0500

    removed a line of whitespace

--- submit.sh
+++ submit.sh
@@ -10,6 +10,5 @@ module load cuda
 module load cudnn/gcc
 module load openmpi/gcc
 
-
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDNN_ROOT/lib64:$CUDA_HOME/lib64:/lustre/ssingh37/Acads/CMSC818x/nccl/build/lib
 NCCL_IB_DISABLE=1 NCCL_SOCKET_IFNAME=eno1 mpirun ./multi_GPU_wfbp
